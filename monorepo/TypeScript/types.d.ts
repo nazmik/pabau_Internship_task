fd1f2810012280025cc9036742e78ca801e8d3bd
NazmiKameri <nazmik1233@gmail.com>
2023-06-14 19:09:47 +0200

    Perfundimi i taskut te Type Script

--- monorepo/TypeScript/types.d.ts
+++ monorepo/TypeScript/types.d.ts
@@ -1,13 +1,19 @@
+interface Skill {
+	skill: 'React' | 'HTML' | 'JS' | 'TypeScript';
+}
+
 interface Intern {
 	name: string;
 	age: number;
 	skills: string[];
+	// skills:Skill[] Used if we want to have certain skills only
 }
 
 interface Junior {
 	name: string;
 	age: number;
 	skills: string[];
+	// skills:Skill[] Used if we want to have certain skills only
 	date_of_promotion: Date;
 	domain: Domain;
 }
